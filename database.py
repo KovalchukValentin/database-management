0e27354526a4b6500063171dde6bff9fd42d4445
KovalchukValentin <kovalchukvalentine@gmail.com>
2023-10-02 22:59:32 +0300

    Added function insert data to db

--- database.py
+++ database.py
@@ -26,6 +26,15 @@ class DatabaseHandler:
         self.cursor.execute(create_table_query)
         self.conn.commit()
 
+    def insert_data(self, brand: str, taste: str, nicotine: int, volume: int, price: int, code: int):
+        insert_query = """
+        INSERT INTO sets (brand, taste, nicotine, volume, price, code)
+        VALUES (?, ?, ?, ?, ?, ?);
+        """
+        values = (brand, taste, nicotine, volume, price, code)
+        self.cursor.execute(insert_query, values)
+        self.conn.commit()
+
     def retrieve_data(self):
         retrieve_query = "SELECT * FROM sets;"
         self.cursor.execute(retrieve_query)
